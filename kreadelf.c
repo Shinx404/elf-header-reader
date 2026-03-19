4f08effae72ffa4bec8aba89e1e15ed525d97717
Redouane Moudjaoui <moudjored@gmail.com>
2026-03-19 13:56:08 +0100

    Everything works, it can recognize ELFs but I have yet to add recognition for other filetypes (possible future change)

--- kreadelf.c
+++ kreadelf.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]){
 	FILE *file;
-	char magic_numbers[4][2];
-	char elf_sign[4][2] = {"7F","45","4C","46"};
-	char *str;
+	char magic_numbers[4];
+	char elf_sign[4] = {0x7F,0x45,0x4C,0x46};
+	int cmp = 1;
 
 	if(argv[1] !=  NULL){
 		file = fopen(argv[1], "rb");
 		if(file != NULL){
+			
 			fread(magic_numbers,sizeof(int),4,file);
 			
 			printf("filetype: ");
 			for(int i = 0; i < sizeof(magic_numbers)/sizeof(magic_numbers[0]);i++){
-				printf(" %s",magic_numbers[i]);
+				printf(" %x",magic_numbers[i]);
+				if(magic_numbers[i] != elf_sign[i]){
+					cmp = 0;
+				}
+			}
+			if(cmp != 0){
+				printf("\nthis is an ELF!");
 			}
 		}	
 		else{
